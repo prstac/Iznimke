06d46038929e6d8fc79f2d1c326b39d61780ba52
josip <josip@laptop>
2025-03-05 20:08:59 +0100

    ralomak

---
+++ src/Iznimke/Razlomak.java
@@ -0,0 +1,67 @@
+package Iznimke;
+
+import java.text.Format;
+
+// https://prnt.sc/Sx24Xe-nXUkb
+public class Razlomak {
+    private int brojnik;
+    private int nazivnik;
+    private int predznak = 1;
+    public Razlomak(int brojnik, int nazivnik) {
+        if (Math.signum(brojnik) != Math.signum(nazivnik)) {
+            predznak = -1;
+        } else {
+            predznak = 1;
+        }
+
+        this.brojnik = Math.abs(brojnik);
+        this.nazivnik = Math.abs(nazivnik);
+
+        if (this.brojnik % this.nazivnik == 0) {
+            this.brojnik /= this.nazivnik;
+            this.nazivnik = 1;
+        } else if (this.nazivnik % this.brojnik == 0) {
+            this.nazivnik /= this.brojnik;
+            this.brojnik = 1;
+        }
+    }
+
+    public Razlomak puta(Razlomak drugi) {
+        return new Razlomak(brojnik * drugi.brojnik * predznak, nazivnik * drugi.nazivnik * drugi.predznak);
+    }
+
+    public Razlomak dijeljeno(Razlomak drugi) {
+        return new Razlomak(brojnik * drugi.nazivnik * predznak, nazivnik * drugi.brojnik * drugi.predznak);
+    }
+
+    public Razlomak plus(Razlomak drugi) {
+        int a = this.brojnik*predznak;
+        if (this.nazivnik == drugi.nazivnik) {
+            a+=drugi.brojnik * drugi.predznak;
+            return new Razlomak(a, this.nazivnik);
+        }
+        else {
+            a =drugi.nazivnik * this.brojnik * predznak + this.nazivnik*drugi.brojnik * drugi.predznak;
+            return new Razlomak(a, drugi.nazivnik * this.nazivnik);
+        }
+    }
+
+    public Razlomak minus(Razlomak drugi) {
+        int a = this.brojnik * predznak;
+        Razlomak rezultat;
+        if (this.nazivnik == drugi.nazivnik) {
+            a-=drugi.brojnik * drugi.predznak;
+            return new Razlomak(a, this.nazivnik);
+        }
+        else {
+            a =drugi.nazivnik * this.brojnik*predznak - this.nazivnik*drugi.brojnik* drugi.predznak;
+            return new Razlomak(a, drugi.nazivnik * this.nazivnik);
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (brojnik == 0) return "0";
+        return (Math.signum(predznak) == 1? "": "-") + brojnik + "/" + nazivnik;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,38 @@
+import Iznimke.Razlomak;
+
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Razlomak prvi, drugi;
+        prvi = unosRazlomka("prvi");
+        drugi = unosRazlomka("drugi");
+
+        System.out.println("Zbroj je : " + prvi.plus(drugi));
+        System.out.println("Razlika je : " + prvi.minus(drugi));
+        System.out.println("Umnozak je : " + prvi.puta(drugi));
+        System.out.println("Kvocijent je : " + prvi.dijeljeno(drugi));
+    }
+
+    public static Razlomak unosRazlomka(String message) {
+        Razlomak razlomak;
+        do {
+            System.out.println("Unesite " + message + " razlomak (brojnik nazivnik)");
+            try {
+                Scanner scanner = new Scanner(System.in);
+                String linija = scanner.nextLine().replaceAll("  "," ").trim();
+                String[] linijaArray = linija.split(" ");
+
+                var brojnik = Integer.parseInt(linijaArray[0]);
+                var nazivnik = Integer.parseInt(linijaArray[1]);
+
+                return new Razlomak(brojnik, nazivnik);
+            } catch (NumberFormatException e) {
+                System.out.println("Niste unijeli ispravan broj");
+            }
+        } while (true);
     }
 }
